111227e7dd4f07e50eb17f2d37d3fb01077c291c
Gustavo <gustavo.alesalrez@outlook.com>
2026-07-08 18:56:50 -0300

    feat: initialize App component with base Jogo interface and state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 
+interface Jogo {
+  id: number;
+  nome: string;
+  genero: string;
+  horasJogadas: number;
+  status: 'Backlog' | 'Jogando' | 'Zerado';
+}
+
 function App() {
+
+  const [jogos, setJogos] = useState<jogo[]>([
+    { id: 1, nome: 'Dying Light', genero: 'Terror', horasJogadas: 50, status: 'Zerado' },
+    { id: 2, nome: 'Forza Horizon 5', genero: 'Corrida', horasJogadas: 20, status: 'Jogando' },
+    { id: 3, nome: 'EA Sports FC 25', genero: 'Esportes', horasJogadas: 1205, status: 'Zerado' }
+
+  ]);
 
   return (
     <>
